c59af71572b4b1107913fcd66670fe42b1de7dda
jaskinner <jon@slashslashinc.com>
2019-08-31 14:12:44 -0400

    Fixed prod build

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './assets/scss/template.scss'
+// import './assets/images/index'
+
+// import 'popper.js'
+import 'bootstrap'
+// import AOS from 'aos'
+// import './assets/js/jquery.singlePageNav'
+// import './assets/js/custom'
+
+// AOS.init()
