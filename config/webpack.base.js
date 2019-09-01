482fe5ecc9076eb8ed7102023d17b34a3d90dfd2
jaskinner <jon@slashslashinc.com>
2019-09-01 16:21:28 -0400

    fix for webpack envs

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -10,68 +10,64 @@ function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
 
-module.exports = env => {
-    const isProduction = env.production === true;
-
-    return {
-        context: path.resolve(__dirname, '../'),
-        entry: {
-            app: './src/index.js'
-        },
-        output: {
-            path: path.resolve(__dirname, '../dist'),
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: '[name].[hash].css',
-                chunkFilename: '[id].[hash].css',
-            }),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                title: 'Faster on Fire | Lessons Learned',
-                template: './src/index.pug',
-                filename: 'index.html',
-                meta: {
-                    //TODO: Add date
-                    'description': 'The Official Website of Raleigh, NC Pop Punk band Faster on Fire, stream Lessons Learned on all major platforms October XX, 2019',
-                    // TODO: Maybe change this
-                    'theme-color': '#000',
-                    'robots': isProduction ? vars.meta.bots.prod : vars.meta.bots.dev,
-                    'googlebot': isProduction ? vars.meta.bots.prod : vars.meta.bots.dev,
-                    'rating': 'General'
-                }
-            }),
-            new FaviconsWebpackPlugin('./src/favicon.svg')
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.(scss|css)$/,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-                    include: [resolve('src'), resolve('node_modules/bootstrap')]
-                },
-                {
-                    test: /\.(png|jpg|JPG|svg)$/,
-                    use: [
-                        {
-                            loader: "url-loader",
-                            options: {
-                                limit: 8192,
-                                name: 'images/[name].[ext]'
-                            }
-                        },
-                        {
-                            loader: "image-webpack-loader",
-                            options: {}
-                        },
-                    ],
-                    include: [resolve('src/images')]
-                },
-                {
-                    test: /\.pug$/,
-                    use: ["pug-loader"]
-                }
-            ]
-        }
+module.exports = {
+    context: path.resolve(__dirname, '../'),
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Faster on Fire | Lessons Learned',
+            template: './src/index.pug',
+            filename: 'index.html',
+            meta: {
+                //TODO: Add date
+                'description': 'The Official Website of Raleigh, NC Pop Punk band Faster on Fire, stream Lessons Learned on all major platforms October XX, 2019',
+                // TODO: Maybe change this
+                'theme-color': '#000',
+                'robots': vars.meta.bots.dev,
+                'googlebot': vars.meta.bots.dev,
+                'rating': 'General'
+            }
+        }),
+        new FaviconsWebpackPlugin('./src/favicon.svg')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+                include: [resolve('src'), resolve('node_modules/bootstrap')]
+            },
+            {
+                test: /\.(png|jpg|JPG|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                            name: 'images/[name].[ext]'
+                        }
+                    },
+                    {
+                        loader: "image-webpack-loader",
+                        options: {}
+                    },
+                ],
+                include: [resolve('src/images')]
+            },
+            {
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            }
+        ]
     }
 };
