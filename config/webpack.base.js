f71f2931301f403f43ba360deaeb451b8f591cac
Jon Skinner <jon.a.skinner1@gmail.com>
2019-09-23 10:31:57 -0400

    webpack killing images

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -64,25 +64,25 @@ module.exports = {
                     {
                         loader: "image-webpack-loader",
                         options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: true,
-                            },
-                            pngquant: {
-                                // quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
+                            // mozjpeg: {
+                            //     progressive: true,
+                            //     quality: 65
+                            // },
+                            // // optipng.enabled: false will disable optipng
+                            // optipng: {
+                            //     enabled: true,
+                            // },
+                            // pngquant: {
+                            //     // quality: 65-80,
+                            //     speed: 11
+                            // },
+                            // gifsicle: {
+                            //     interlaced: false,
+                            // },
+                            // // the webp option will enable WEBP
+                            // webp: {
+                            //     quality: 75
+                            // }
                         }
                     },
                 ],
