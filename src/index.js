fa6866766c383e0d58ac646bc726d2092db26732
Jon Skinner <jon.a.skinner1@gmail.com>
2019-11-25 14:23:59 -0500

    Short term post LL release changes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import './scss'
 import $ from 'jquery'
 import './js'
-// import 'jquery-scroll-lock'
 import AOS from "aos/dist/aos"
 
 (function () {
-    // $('*').scrollLock();
 
     AOS.init({
         disable: 'mobile'
     })
-})()
-
-// $(window).on('load', function () {
-//     if ($('div').is('.cta-overlay')) {
-//         $('.cta-overlay').delay(500).fadeOut(800);
-//     }
-//
-//     setTimeout(function () {
-//         $('*').scrollLock('disable')
-//     }, 1200);
-//
-// });
+})();
 
 $('.nav-page-links a').click(function (e) {
     var anchor = $(this).attr('href');
     e.preventDefault();
 
     $.scrollTo(anchor, 400);
-})
+});
 
 $(function () {
     $('.player-buttons button').click(function () {
@@ -39,11 +26,11 @@ $(function () {
 
         $('#music').find('.active').removeClass('active');
 
-        player.addClass('active')
-        $this.addClass('active')
-    })
+        player.addClass('active');
+        $this.addClass('active');
+    });
 
     $('.toggler').click(function () {
         $('#navbar').toggleClass('nav-open')
     })
-})
+});
